a6fb226cc261c9360d8b3d49ab841de40f0a72d5
Vishal Pachori <vishalpachori25@gmail.com>
2024-09-18 11:11:07 +0530

    Added config & Contacts

--- src/constants.ts
+++ src/constants.ts
@@ -2,166 +2,178 @@ import { User } from "./types";
 
 export const contactList: Array<User> = [
   {
+    id: 24,
+    type: "Plumber",
+    name: "Amar",
+    phoneNumber: "+918088378285",
+    description: "(Weekly off  Wednesday)"
+  },
+  {
     id: 1,
     type: "Carpenter",
     name: "Amit Carpenter Bhuvi_Carpenter",
     phoneNumber: "+91 72594 13559",
-    description: "",
-  },
-  {
-    id: 2,
-    type: "Gardener",
-    name: "Bhuvi Gardner Good Nirmal_Gardener",
-    phoneNumber: "8073911236",
-    description: "",
+    description: ""
   },
   {
     id: 3,
     type: "UPVC Window",
     name: "Asif_UPVC Window",
     phoneNumber: "+91 88841 53715",
-    description: "",
+    description: ""
+  },
+  {
+    id: 2,
+    type: "Gardener",
+    name: "Bhuvi Gardner Good Nirmal_Gardener",
+    phoneNumber: "91 8073911236'",
+    description: ""
   },
   {
     id: 4,
     type: "Car Cleaner",
     name: "Bikash Kumar Car Clean Bhuvi_Car Cleaner",
-    phoneNumber: "917022000000",
-    description: "",
-  },
-
-  {
-    id: 6,
-    type: "Electrician",
-    name: "Sadham _Electrician",
-    phoneNumber: "918801000000",
-    description: "9359161961",
+    phoneNumber: "+917022000000",
+    description: ""
   },
   {
     id: 7,
     type: "Golgappa",
     name: "Golgappa Bhuvi _Golgappa",
-
     phoneNumber: "+91 78994 38617",
-    description: "",
+    description: ""
+  },
+  {
+    id: 5,
+    type: "HouseKeeping",
+    name: "House Keeping Supervisor",
+    phoneNumber: "+91 93656 46289",
+    description: ""
   },
   {
     id: 8,
     type: "Indane",
     name: "Indane Delivery Ramesh_Indane",
-
     phoneNumber: "+91 97428 76230",
-    description: "",
-  },
-  {
-    id: 9,
-    type: "Cloth Iron",
-    name: "Shekhar Bhuvi_Cloth Iron",
-
-    phoneNumber: "+91 88617 54305",
-    description: "",
+    description: ""
   },
   {
     id: 10,
     type: "Gardner",
     name: "Karan_Gardner",
-
     phoneNumber: "+91 96961 54622",
-    description: "",
+    description: ""
   },
   {
     id: 11,
     type: "Bathroom Cleaner UC",
     name: "Kaviraj Bhuvi_Bathroom Cleaner UC",
-
     phoneNumber: "96069 83699",
-    description: "",
+    description: ""
+  },
+  {
+    id: 23,
+    type: "Security",
+    name: "Main Gate Security",
+    phoneNumber: "+919980951853",
+    description: ""
   },
   {
     id: 12,
     type: "FM",
     name: "Manju Bhuvi FM_FM",
-
     phoneNumber: "+91 99004 03767",
-    description: "",
+    description: ""
   },
   {
     id: 13,
     type: "Electrician",
     name: "Minar_Electrician",
-
     phoneNumber: "77954 74032",
-    description: "",
+    description: ""
+  },
+  {
+    id: 25,
+    type: "Electrician",
+    name: "Minaruddin",
+    phoneNumber: "+917795474032",
+    description: "(Weekly off  Tuesday)"
   },
   {
     id: 14,
     type: "Gardener",
     name: "Mohir_Gardener",
-
     phoneNumber: "+91 69019 94520",
-    description: "",
+    description: ""
   },
   {
     id: 15,
     type: "Plumber",
     name: "Niranjan Plumber Bhuvi_Plumber",
-
     phoneNumber: "96061 94516",
-    description: "",
+    description: ""
   },
   {
     id: 16,
     type: "Tiles",
     name: "Noar Tiles Granite_Tiles",
-
     phoneNumber: "+918006000000",
-    description: "",
+    description: ""
   },
   {
     id: 17,
     type: "Cloth Iron",
     name: "Rahul_Iron Clothes",
-
     phoneNumber: "+917985000000",
-    description: "",
+    description: ""
+  },
+  {
+    id: 6,
+    type: "Electrician",
+    name: "Sadham _Electrician",
+    phoneNumber: "918801000000",
+    description: "9359161961"
   },
   {
     id: 18,
     type: "Househelp Bhuvi",
     name: "Sarima_Househelp Bhuvi",
-
     phoneNumber: "+91 73497 25706",
-    description: "",
+    description: ""
+  },
+  {
+    id: 9,
+    type: "Cloth Iron",
+    name: "Shekhar Bhuvi_Cloth Iron",
+    phoneNumber: "+91 88617 54305",
+    description: ""
   },
   {
     id: 19,
     type: "Tent House",
     name: "SLV Tent   _Tent House",
-
     phoneNumber: "9880424984",
-    description: "",
+    description: ""
   },
   {
     id: 20,
     type: "Electrician",
     name: "Sujit_Electrician",
-
     phoneNumber: "90661 44029",
-    description: "",
+    description: ""
   },
   {
     id: 21,
     type: "Househelp Bhuvi",
     name: "Tulsi_Househelp Bhuvi",
-
     phoneNumber: "+918618000000",
-    description: "",
+    description: ""
   },
   {
     id: 22,
     type: "Househelp Bhuvi",
     name: "Yasodha_Househelp Bhuvi",
     phoneNumber: "+91 87541 99155",
-    description: "",
-  },
+    description: ""
+  }
 ];
